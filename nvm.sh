2f173dcc32150df0b89dbbb6fa3b176e88068979
peejayaccts <peejayaccts@gmail.com>
2017-04-02 15:23:17 +0800

    Edit Readme.md

--- nvm.sh
+++ nvm.sh
@@ -3,14 +3,14 @@ sudo apt-get install build-essential libssl-dev
 
 # import nvm
 curl https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash 
-source ~/.profile
+\source ~/.profile
 
 # see list of node
-nvm ls-remote
+\nvm ls-remote
 
 # install version
-nvm install v6.10.1
+\nvm install v6.10.1
 
 # to make nodejs run on port 80
-sudo apt-get install libcap2-bin
-sudo setcap cap_net_bind_service=+ep `readlink -f \`which node\``
+\sudo apt-get install libcap2-bin
+\sudo setcap cap_net_bind_service=+ep `readlink -f \`which node\``
